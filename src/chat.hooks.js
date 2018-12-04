6f099ecfc0ff5f0c31af5927c526cbba5149ac63
evan <evan@rangle.io>
2018-12-04 17:49:32 -0500

    meh

--- src/chat.hooks.js
+++ src/chat.hooks.js
@@ -1,39 +1,39 @@
-import { useState, useEffect } from "react";
-import { getChatEngine } from "./chat-engine";
+import { useState, useEffect } from 'react';
+import { getChatEngine } from './chat-engine';
 
 const ChatEngine = getChatEngine();
 
 export function withChatInput() {
-  const [chatInput, setChatInput] = useState("");
+  const [chatInput, setChatInput] = useState('');
 
   return [chatInput, setChatInput];
 }
 
 export function withMessages() {
-  const [messages, setMessages] = useState([]);
+  let [messages, setMessages] = useState([]);
 
-  const handleMessage = (payload) => {
+  const handleMessage = payload => {
     setMessages([
-        ...messages,
-        {
-          sender: payload.sender.uuid,
-          text: payload.data.text
-        }
-      ]);
-  }
+      ...messages,
+      {
+        sender: payload.sender.uuid,
+        text: payload.data.text
+      }
+    ]);
+  };
 
   useEffect(() => {
-    ChatEngine.global.on("message", handleMessage);
-    
+    ChatEngine.global.on('message', handleMessage);
+
     return () => {
-        ChatEngine.global.off("message", handleMessage);
-    }
+      ChatEngine.global.off('message', handleMessage);
+    };
   });
-  
+
   function sendMessage(message) {
-    ChatEngine.global.emit("message", {
-        text: message
-      });
+    ChatEngine.global.emit('message', {
+      text: message
+    });
   }
-  return [messages, sendMessage]
+  return [messages, sendMessage];
 }
